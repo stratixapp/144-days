987c6712e201365389142b3666821119d9581bfa
Ananthu <stratixapp@gmail.com>
2026-08-10 13:48:26 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,52 @@
+const CACHE_NAME = "144days-cache-v2";
+
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./photo.jpg",
+  "./day-photo-1.jpg",
+  "./day-photo-2.jpg",
+  "./day-photo-3.jpg",
+  "./clock-face.jpg",
+  "./icon-192.png",
+  "./icon-512.png",
+  "./apple-touch-icon.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return (
+        cached ||
+        fetch(event.request)
+          .then((response) => {
+            const copy = response.clone();
+            caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+            return response;
+          })
+          .catch(() => cached)
+      );
+    })
+  );
+});
